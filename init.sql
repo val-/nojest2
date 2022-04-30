99ac90634bd1cf260bb8a0746bcf37d9d105f912
val- <webkoder@ya.ru>
2022-04-30 06:44:54 +0300

    DB refactor

--- init.sql
+++ init.sql
@@ -16,6 +16,11 @@ CREATE TYPE nj_ject_status AS ENUM (
     'INACTIVE'
 );
 
+CREATE TYPE nj_version_status AS ENUM (
+    'ACTIVE',
+    'INACTIVE'
+);
+
 CREATE TYPE nj_task_status AS ENUM (
     'OPENED',
     'ASSIGNED',
@@ -26,29 +31,55 @@ CREATE TYPE nj_task_status AS ENUM (
 );
 
 CREATE TABLE nj_user(
-  id SERIAL PRIMARY KEY,
-  full_name varchar(255),
-  phone_number varchar(30),
-  email varchar(255) NOT NULL UNIQUE,
-  email_confirmed boolean,
-  email_confirm_token varchar(64),
-  password_hash varchar(64) NOT NULL,
-  avatar text
+    id SERIAL PRIMARY KEY,
+    full_name varchar(255),
+    phone_number varchar(30),
+    email varchar(255) NOT NULL UNIQUE,
+    email_confirmed boolean,
+    email_confirm_token varchar(64),
+    password_hash varchar(64) NOT NULL,
+    avatar text
 );
 
 CREATE TABLE nj_ject(
     id SERIAL PRIMARY KEY,
-    ject_author_id integer REFERENCES nj_user(id),
     title varchar(255),
     description text,
     status nj_ject_status
+);
+
+CREATE TABLE nj_member(
+    id SERIAL PRIMARY KEY,
+    ject_id integer REFERENCES nj_ject(id),
+    user_id integer REFERENCES nj_user(id)
+);
+
+CREATE TABLE nj_role(
+    id SERIAL PRIMARY KEY,
+    name varchar(255)
+);
+
+CREATE TABLE nj_member_role(
+    id SERIAL PRIMARY KEY,
+    member_id integer REFERENCES nj_member(id),
+    role_id integer REFERENCES nj_role(id)
+);
+
+CREATE TABLE nj_version(
+    id SERIAL PRIMARY KEY,
+    ject_id integer REFERENCES nj_ject(id),
+    title varchar(255),
+    description text,
+    status nj_version_status
 );
 
 CREATE TABLE nj_task(
     id SERIAL PRIMARY KEY,
     ject_id integer REFERENCES nj_ject(id),
     task_author_id integer REFERENCES nj_user(id),
-    contractor_id integer REFERENCES nj_user(id)
+    contractor_id integer REFERENCES nj_user(id),
+    parent_id integer REFERENCES nj_task(id),
+    target_version_id integer REFERENCES nj_version(id)
 );
 
 CREATE TABLE nj_task_history(
