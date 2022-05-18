ed27cbff85a746aeba46f667a2c8a95ff20681cb
val- <webkoder@ya.ru>
2022-05-18 13:48:50 +0300

    SQL refactor

--- sql/mock-tasks.sql
+++ sql/mock-tasks.sql
@@ -2,62 +2,63 @@
 -- FILLIN NOJEST APP TASKS
 
 INSERT INTO nj_task(
-
-  title
-  description
-  ject_id
-  task_author_id
-  contractor_id
-  deadline
-  parent_id
+  title,
+  description,
+  ject_id,
+  task_author_id,
+  contractor_id,
+  deadline,
+  parent_id,
   target_version_id
-
 )
 VALUES
 (
-  'A poject example title',
-  'A poject example description',
-  0,
+  'A task example title',
+  'A task example description',
   1,
-  1652868489772,
-  -1,
-  -1
-),
-
-
-(
-  'B poject example title',
-  'B poject example description',
-  0,
   1,
-  1652868489772,
-  -1,
-  -1
+  2,
+  TIMESTAMP '2011-05-23 15:36:38',
+  null,
+  null
 ),
 (
-  'C poject example title',
-  'C poject example description',
-  0,
+  'B task example title',
+  'B task example description',
   1,
-  1652868489772,
-  -1,
-  -1
+  1,
+  2,
+  TIMESTAMP '2011-05-23 15:36:38',
+  1,
+  null
 ),
 (
-  'D poject example title',
-  'D poject example description',
-  0,
-  1,
-  1652868489772,
-  0,
-  -1
+  'C task example title',
+  'C task example description',
+  2,
+  3,
+  4,
+  TIMESTAMP '2011-05-23 15:36:38',
+  null,
+  null
 ),
 (
-  'E poject example title',
-  'E poject example description',
-  0,
-  1,
-  1652868489772,
-  1,
-  -1
+  'D task example title',
+  'D task example description',
+  2,
+  3,
+  4,
+  TIMESTAMP '2011-05-23 15:36:38',
+  3,
+  null
+),
+(
+  'E task example title',
+  'E task example description',
+  2,
+  3,
+  4,
+  TIMESTAMP '2011-05-23 15:36:38',
+  4,
+  null
 );
