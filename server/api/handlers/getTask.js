7d7f7b05bc2e1fef01a30003c039bb79038df323
val- <webkoder@ya.ru>
2022-05-19 13:00:26 +0300

    getTaskById refactor

--- server/api/handlers/getTask.js
+++ server/api/handlers/getTask.js
@@ -3,8 +3,6 @@ const Task = require('../../models/task');
 
 module.exports = (req, res) => {
 
-    const { authorizedUser } = req.session;
-
     Task.getTaskById(req.params.taskId).then(task => {
 
         Ject.get(task.jectId).then(ject => {
@@ -13,29 +11,6 @@ module.exports = (req, res) => {
                 ...task,
                 ject,
             });
-/*
-            const ownJect = authorizedUser.id === ject.authorId;
-            const ownTask = authorizedUser.id === task.contractorId;
-            const { isModerator } = authorizedUser;
-            let statusFlow = false;
-
-            if (isModerator) {
-                statusFlow = Task.statusFlowByModerator;
-            } else if (ownJect) {
-                statusFlow = Task.statusFlowByCustomer;
-            } else if (ownTask) {
-                statusFlow = Task.statusFlowByContractor;
-            }
-            if (statusFlow) {
-                res.json({
-                    ...task,
-                    ject,
-                    nextStatusVariants: statusFlow[task.status],
-                });
-            } else {
-                res.json({ error: 'You have no permissions to view this task' });
-            }
-*/
 
         }).catch(error => {
             res.json({ error });
